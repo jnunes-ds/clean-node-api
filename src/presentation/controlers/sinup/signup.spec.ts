81214c4491a4bd4cd19e0d9cbd2873f6509fa273
Júnior  Nunes <denisonprobable13@gmail.com>
2023-09-09 19:35:20 -0300

    refactor: move all signup protocols to a single file

--- src/presentation/controlers/sinup/signup.spec.ts
+++ src/presentation/controlers/sinup/signup.spec.ts
@@ -1,8 +1,8 @@
 import { SignUpController } from './signup'
-import { MissingParamError, InvalidParamError, ServerError } from "../errors";
-import { type EmailValidator } from '../protocols';
-import { type AddAccount, type AddAccountModel } from '../../domain/usecases/add-account';
-import { type AccountModel } from '../../domain/models/account';
+import { MissingParamError, InvalidParamError, ServerError } from "../../errors";
+import { type AddAccount, type AddAccountModel } from '../../../domain/usecases/add-account';
+import { type AccountModel } from '../../../domain/models/account';
+import { type EmailValidator } from '../../protocols/email-validator';
 
 interface IMakeSut {
   sut: SignUpController
--- src/presentation/controlers/sinup/signup.ts
+++ src/presentation/controlers/sinup/signup.ts
@@ -1,16 +1,12 @@
-import { type AddAccount } from "../../domain/usecases/add-account";
+import { InvalidParamError, MissingParamError } from "../../errors";
+import { badRequest, serverError } from "../../helpers/http-helper";
 import {
-  MissingParamError,
-  InvalidParamError
-} from "../errors";
-import { badRequest, serverError } from '../helpers/http-helper'
-
-import {
+  type AddAccount,
   type Controller,
   type EmailValidator,
   type HttpRequest,
   type HttpResponse
-} from "../protocols";
+} from "./signup-protocols";
 
 export class SignUpController implements Controller {
   constructor (
